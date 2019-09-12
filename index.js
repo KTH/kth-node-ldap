1ad5930c96cfadac240883a0a33a8e359a4c5d88
Viktor Atterlönn <viktor_atterlonn@live.se>
2019-09-12 15:43:18 +0200

    Export correct function to createClient

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  createClient: require('./lib/ldap/client'),
+  createClient: require('./lib/ldap/client').createClient,
   filters: require('./lib/filters'),
   utils: require('./lib/utils'),
   getSessionUserHelpers: require('./lib/session.js')
