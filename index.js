5e905c9e000975c60ecdcf0233291dbab78c5c00
Niklas Sandström <mr.n.sandstrom@gmail.com>
2023-09-05 15:56:44 +0200

    move entryfile

--- index.js
+++
@@ -1,9 +0,0 @@
-const { createClient, search, searchOne } = require('./lib/ldapts/ldaptsClient')
-
-module.exports = {
-  createClient,
-  filters: require('./lib/filters'),
-  utils: require('./lib/utils'),
-  search,
-  searchOne,
-}
