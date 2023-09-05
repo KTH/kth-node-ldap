0ecce5ae406cd27e9bf4d0c63802d800fb280bca
Niklas Sandström <mr.n.sandstrom@gmail.com>
2023-09-05 15:38:09 +0200

    cleanup jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  ...require('./tests/jest-common'),
-  projects: ['./tests/jest.server.js'],
   verbose: true,
   testPathIgnorePatterns: ['/node_modules/', '/__mocha_tests__/'],
 }
