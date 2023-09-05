10e9d65386fe8782ccc4e09206845690355bb438
Niklas Sandström <mr.n.sandstrom@gmail.com>
2023-09-05 16:01:02 +0200

    remove seemingly unused session-helper

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ module.exports = {
   createClient,
   filters: require('./lib/filters'),
   utils: require('./lib/utils'),
-  getSessionUserHelpers: require('./lib/session.js'),
   search,
   searchOne,
 }
